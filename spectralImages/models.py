cdfdff22d66b54dc04dd54885422639bc066204e
Khawar Ali <78090293+khawar-ali-brainx@users.noreply.github.com>
2022-11-17 03:08:08 +0200

    Scuessfull to load images into table

--- spectralImages/models.py
+++ spectralImages/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 class SpectralImage(models.Model):
     title = models.CharField(max_length=100)
     path = models.CharField(max_length=150)
-    date_posted = models.DateTimeField(default=timezone.now)
+    date_added = models.DateTimeField(default=timezone.now)
 
-    def __str__(self):
-        return self.title
+    # def __int__(self):
+    #     return self.id
--- spectralImages/views.py
+++ spectralImages/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from .models import SpectralImage
 from .spectral_tiffs import read_stiff
-import matplotlib.pyplot as plt
-from django.http import HttpResponse
-import numpy as np
-from os import listdir
-from os.path import isfile, join
+# import matplotlib.pyplot as plt
+# from django.http import HttpResponse
+# import numpy as np
+# from os import listdir
+# from os.path import isfile, join
+import os
 
 
 spectralImages = [
@@ -24,15 +25,48 @@ spectralImages = [
 cube, wavelengths, preview_image, metadata = read_stiff(
     "spectralImages/Set 1, lower 2, icg.tif")
 
+def getFileNameFromFolder():
+    path = "spectralImages/flat-fielded"
+    return os.listdir(path)
 
+
+def getNameOfFileWithOutExtension(file):
+    return os.path.splitext(file)[0]
+
+def saveDataIntoTable(files):
+    spectralImages = SpectralImage.objects
+    if not spectralImages.exists():
+        for index, file in enumerate(files):
+            image = SpectralImage(index, getNameOfFileWithOutExtension(file), file)
+            image.save()
+    else :
+        print("Already added in database")
+
+            
 def home(request):
+    files = getFileNameFromFolder()
+    saveDataIntoTable(files)
+    
     context = {
-        'spectralImages': spectralImages #SpectralImage.objects.all()
+        'spectralImages': SpectralImage.objects.all(),
     }
     # onlyfiles = [f for f in listdir(
         # 'spectralImages') if isfile(join('spectralImages', f))]
 
-    # print(onlyfiles)
+    # print(cube, wavelengths, preview_image, metadata)
+    # print(cube)
+    f = open("demofile.txt", "w")
+    # for file in files:
+    #     print(getNameOfFileWithOutExtension(file))
+    # print(len(cube))
+    # print(len(cube[0]))
+    # print(len(cube[0][0]))
+    # for value in cube:
+    #     print(len(value))
+    #     for item in value:
+    #         print(len(item))
+            # f.write(str(item))
+    # print(f.read())
     return render(request, 'spectralImages/home.html', context)
     # plt.plot(wavelengths, cube[511, 511, :])
     # return HttpResponse("<h1>Test</h1>")
