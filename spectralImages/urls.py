7324b505211d147bda9a44cd49fe91bc583019e8
Khawar Ali <78090293+khawar-ali-brainx@users.noreply.github.com>
2022-11-18 00:43:37 +0200

    loaded Images and data from the tif files

--- spectralImages/urls.py
+++ spectralImages/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from spectralImages.views import fetchDataFromSelected
 
 urlpatterns = [
     path("", views.home, name='spectralImages-home'),
+    path('fetchDataFromSelected/<image>/', fetchDataFromSelected, name='select_item_view'),
 ]
--- spectralImages/views.py
+++ spectralImages/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from .models import SpectralImage, BinaryMasksImage
 from .spectral_tiffs import read_stiff
 import os
+from django.http import HttpResponse
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL
+from io import BytesIO
+import base64
 
 #read sample image for testing
 cube, wavelengths, preview_image, metadata = read_stiff(
@@ -35,12 +41,34 @@ def saveBinaryMaskDataIntoTable(files):
     else :
         print("Already added in database")     
 
+
+def fetchDataFromSelected(request, image):
+    cube, wavelengths, preview_image, metadata = read_stiff("spectralImages/flat-fielded/"+image+".tif")
+    
+    image_array = np.array(preview_image, dtype=np.uint8)
+    img = PIL.Image.fromarray(image_array)
+    buffer = BytesIO()
+    img.save(buffer, "PNG")
+    contents = base64.b64encode(buffer.getvalue()).decode('utf-8')
+    dataurl = 'data:image/png;base64,' + contents
+    # img.show()
+    
+    context = {
+        'spectralImages': SpectralImage.objects.all(),
+        'cube': cube,
+        'wavelengths': wavelengths,
+        'preview_image': preview_image,
+        'metadata': metadata,
+        'dataurl': dataurl,
+    }
+    return render(request, 'spectralImages/home.html', context)
+
 def home(request):
     flatFieldFiles = getFlatFieldedImagesNameFromFolder()
     saveFlatFieldedDataIntoTable(flatFieldFiles)
     binaryMasksFiles = getBinaryMasksImagesNameFromFolder()
     saveBinaryMaskDataIntoTable(binaryMasksFiles)
-
+    
     context = {
         'spectralImages': SpectralImage.objects.all(),
     }
