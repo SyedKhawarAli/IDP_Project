e2804c525f6eaa65163c8b32b0c8fdd1d7e29750
Khawar Ali <khawarali5@gmail.com>
2022-11-25 05:04:44 +0200

    Showing both spectral and its mask image

--- spectralImages/models.py
+++ spectralImages/models.py
@@ -1,14 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
-class SpectralImage(models.Model):
-    title = models.CharField(max_length=100)
-    path = models.CharField(max_length=150)
-    date_added = models.DateTimeField(default=timezone.now)
 
-    def __str__(self):
-        return self.title
-    
 class BinaryMasksImage(models.Model):
     title = models.CharField(max_length=100)
     path = models.CharField(max_length=150)
@@ -16,3 +9,12 @@ class BinaryMasksImage(models.Model):
 
     def __str__(self):
         return self.title
+class SpectralImage(models.Model):
+    title = models.CharField(max_length=100)
+    path = models.CharField(max_length=150)
+    mask_image = models.ForeignKey(BinaryMasksImage, on_delete=models.CASCADE, null=True, blank=True)
+    date_added = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.title
+    
